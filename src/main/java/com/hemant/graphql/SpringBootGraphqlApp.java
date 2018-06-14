f17fc292a4f31be3f6cf4a822bb37e32007be4f0
hemant <hemant.nagpure@concirrus.com>
2018-06-14 12:22:09 +0530

    Main App

---
+++ src/main/java/com/hemant/graphql/SpringBootGraphqlApp.java
@@ -0,0 +1,12 @@
+package com.hemant.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlApp.class, args);
+	}
+}
