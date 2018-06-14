e69c59f2184af7ee9d29d3212d709bb35cac54e2
hemant <hemant.nagpure@concirrus.com>
2018-06-14 12:25:14 +0530

    Added query and mutation resolvers

---
+++ src/main/java/com/hemant/graphql/model/resolvers/MutationResolver.java
@@ -0,0 +1,10 @@
+package com.hemant.graphql.model.resolvers;
+
+import org.springframework.stereotype.Service;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+
+@Service
+public class MutationResolver implements GraphQLMutationResolver {
+
+}
---
+++ src/main/java/com/hemant/graphql/model/resolvers/QueryResolver.java
@@ -0,0 +1,10 @@
+package com.hemant.graphql.model.resolvers;
+
+import org.springframework.stereotype.Service;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+@Service
+public class QueryResolver implements GraphQLQueryResolver {
+
+}
