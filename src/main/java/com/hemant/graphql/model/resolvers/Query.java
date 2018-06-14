46182f5f5137a6928da023f1bb3f3144b53bbdc5
hemant <hemant.nagpure@concirrus.com>
2018-06-15 00:20:29 +0530

    Added implementation of a method

--- src/main/java/com/hemant/graphql/model/resolvers/Query.java
+++ src/main/java/com/hemant/graphql/model/resolvers/Query.java
@@ -1,10 +1,17 @@
 package com.hemant.graphql.model.resolvers;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.hemant.graphql.model.Article;
 
 @Service
-public class QueryResolver implements GraphQLQueryResolver {
+public class Query implements GraphQLQueryResolver {
 
+	public List<Article> getAllArticles(String id) {
+		return Collections.emptyList();
+	}
 }
